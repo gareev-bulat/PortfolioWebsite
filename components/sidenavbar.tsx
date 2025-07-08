50c2e7baeb5165906e4955382c4ae9d1c756499a
Булат Гареев <bulatgareev@MacBook-Air-Bulat.local>
2025-07-08 17:34:57 +0400

    sidenavbar align for mobile screens

--- components/sidenavbar.tsx
+++ components/sidenavbar.tsx
@@ -4,8 +4,22 @@ import Link from "next/link";
 
 const SideNavBar = () => {
   return (
-    <div className='fixed z-10 bottom-0 left-1/2 transform -translate-x-1/2 mb-20 p-5 px-10 h-auto rounded-lg flex flex-col justify-center bg-gradient-to-b from-brand-200 to-brand-500/20 backdrop-blur-md '>
-        <ul className='flex flex-row space-x-10 text-white text-base items-center'>
+    <div className='fixed 
+                    z-10 
+                    bottom-0 
+                    left-1/2 
+                    transform 
+                    -translate-x-1/2 
+                    mb-20 
+                    p-2 px-8 
+                    md:p-4
+                    sm:px-6
+                    h-auto
+                    rounded-lg 
+                    flex flex-col 
+                    justify-center 
+                    bg-gradient-to-b from-brand-200 to-brand-500/20 backdrop-blur-md '>
+        <ul className='flex flex-row space-x-10 md:space-x-10 md:px-4 text-white text-sm md:text-base items-center'>
             <li><Link href="#hero">HERO</Link></li>
             <li><Link href="#aboutme">ABOUT</Link></li>
             <li><Link href="#portfolio">PORTFOLIO</Link></li>
